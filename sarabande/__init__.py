4943b1365c77250ceac02ca30fb9ae741dd4fba8
James <jamessunseri@berkeley.edu>
2022-02-21 02:29:58 -0800

    cleaned

--- sarabande/__init__.py
+++ sarabande/__init__.py
@@ -1,4 +1,4 @@
-'''
+"""
 -----------
 Sarabande - 0.0.1
 ---------------------------------------------
@@ -9,7 +9,7 @@ License: MIT
 Tool for measuring 3/4 PCFs on discrete periodic data..
 ---------------------------------------------
 
-'''
+"""
 
 import numpy as np
 from subprocess import call
@@ -21,7 +21,7 @@ from .full_PCF import calc_zeta
 from .full_PCF_mapped import calc_zeta_mapped
 from .full_PCF_parallel import calc_zeta_parallel
 
-__uri__ = "pending" 
+__uri__ = "pending"
 __author__ = "J. Sunseri"
 __maintainer__ = "J. Sunseri"
 __email__ = "jamessunseri@berkeley.edu"
@@ -30,19 +30,20 @@ __version__ = "0.0.1-alpha"
 __release__ = "0.0.1-alpha"
 __description__ = "Tool for measuring 3/4 PCFs on discrete periodic data."
 
+
 def about():
-    text =  "-------------------------------\n"
+    text = "-------------------------------\n"
     text += "-    Welcome to Sarabande!    -\n"
     text += "-------------------------------\n"
-    text += "Version: " + __version__ + '\n'
-    text += "Authors: " + __author__ + '\n'
+    text += "Version: " + __version__ + "\n"
+    text += "Authors: " + __author__ + "\n"
     text += "-------------------------------\n"
     text += "Download the latest version from: pending\n"
     text += "Report issues to: https://github.com/James11222/sarabande\n"
-    text += "Read the documentations at: " + __uri__ + '\n'
+    text += "Read the documentations at: " + __uri__ + "\n"
     text += "-------------------------------\n"
     print(text)
 
-if __name__ == '__main__':
-    about()
 
+if __name__ == "__main__":
+    about()
